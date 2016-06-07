650aef321a49f186966477ffd24f78b6be92c95f
Czesio <grzesiek_czesio@o2.pl>
2016-06-07 19:42:15 +0200

    Added AudioPlayer implementation

---
+++ src/Luma.SmartHub.Audio.Bass/AudioPlayer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Luma.SmartHub.Audio.Playback;
+
+namespace Luma.SmartHub.Audio.Bass
+{
+    public class AudioPlayer : IAudioPlayer
+    {
+        IEnumerable<IPlayback> IAudioPlayer.Playbacks => Playbacks;
+
+        public IAudioHub AudioHub { get; }
+
+        List<IPlayback> Playbacks { get; }
+
+        public AudioPlayer(IAudioHub audioHub)
+        {
+            AudioHub = audioHub;
+            Playbacks = new List<IPlayback>();
+        }
+
+        public void AddPlayback(IPlayback playback)
+        {
+            Playbacks.Add(playback);
+        }
+
+        public void RemovePlayback(IPlayback playback)
+        {
+            Playbacks.Remove(playback);
+
+            var disposable = playback as IDisposable;
+            disposable?.Dispose();
+        }
+    }
+}
